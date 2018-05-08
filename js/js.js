5cd66057df1cc8eb3ed4851a6199cf72ce9548c2
PedroDpsweb <32308504+PedroDpsweb@users.noreply.github.com>
2018-05-08 21:01:48 +0200

    carga final de hoy

--- js/js.js
+++ js/js.js
@@ -2,16 +2,15 @@ $(function () {
 
     $(".cargamas").click(function () {
 
-        $.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/d0ab4a08/json/json.json", function (json) {
+        $.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/e125e4d7/json/json.json", function (json) {
             
         console.log(json);
         $.each(json, function (i, noticia) {
                 var titulo=noticia.titulo;
-                alert(titulo);
                 var imagen=noticia.imagen;
                 var desc=noticia.descripcion;
 
-                $("#masnoticias").append('"<div class="row "><div class="col-sm-4 miniatura"><a href="not-cardio.html"><img class="img-fluid " src='+imagen+' class="rounded " alt="Cinque Terre "></a></div><div class="col-sm-8" style="background-color:silver; "><div class="desc"><h2>No todo es cardio</h2><p>Descripcion perso.</p></div><div class="row"><div class="col-sm-4 text center"><a href="#"><i class="fab fa-facebook-square fa-2x"></i></i></a><a href="#"><i class="fab fa-twitter-square fa-2x"></i></i></a><a href="#"><i class="fab fa-google-plus-square fa-2x"></i></i></a></div></div></div></div>"')
+                $("#masnoticias").append('"<div class="row "><div class="col-sm-4 miniatura"><a href="not-cardio.html"><img class="img-fluid " src='+imagen+' class="rounded " alt="Cinque Terre "></a></div><div class="col-sm-8" style="background-color:silver; "><div class="desc"><h2>'+titulo+'</h2><p>'+desc+'</p></div><div class="row"><div class="col-sm-4 text center"><a href="#"><i class="fab fa-facebook-square fa-2x"></i></i></a><a href="#"><i class="fab fa-twitter-square fa-2x"></i></i></a><a href="#"><i class="fab fa-google-plus-square fa-2x"></i></i></a></div></div></div></div>"')
 
 
 ;
