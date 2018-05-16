036defd535ec34d08e89bd9386fad9b830a87426
PedroDpsweb <32308504+PedroDpsweb@users.noreply.github.com>
2018-05-16 18:23:43 +0200

    update

--- js/js.js
+++ js/js.js
@@ -6,12 +6,27 @@ $(function () {
 
 
     $(window).scroll(function () {
-        if ($(document).scrollTop() + window.innerHeight >= document.getElementsByTagName("body")[0].scrollHeight) {
+        if ($(document).scrollTop() + window.innerHeight +200 >= document.getElementsByTagName("body")[0].scrollHeight) {
             //logic when user has scrolled to the bottom of the page
-            alert("tamoh redy");
             if (activado == true) {
-                cargado();
+                if(carga1==true){
+                    cargado1();
+                }else if(carga2==true){
+                    cargado2();
+                }
+                
             }
+        }
+    });
+
+    $(".noscroll").click(function(){
+        if (activado == false) {
+            if(carga1==true){
+                cargado1();
+            }else if(carga2==true){
+                cargado2();
+            }
+            
         }
     });
 
@@ -24,15 +39,34 @@ $(function () {
         if (activado == true) {
             $("#switch").html('<button type="button" class="btn btn-danger carga"></button>');
             activado = false;
+            $("#myModal").modal({backdrop: true});
+
         } else {
             $("#switch").html('<button type="button" class="btn btn-success carga"></button>');
             activado = true;
+            $("#myModal1").modal({backdrop: true});
         }
     });
 
     $(".cargamas").click(function () {
 
-        $.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/e125e4d7/json/json.json", function (json) {
+    console.log(json);
+            $.each(json, function (i, noticia) {
+                var titulo = noticia.titulo;
+                var imagen = noticia.imagen;
+                var desc = noticia.descripcion;
+
+                $("#masnoticias").append('"<div class="row "><div class="col-sm-4 miniatura"><a href="not-cardio.html"><img class="img-fluid " src=' + imagen + ' class="rounded " alt="Cinque Terre "></a></div><div class="col-sm-8" style="background-color:silver; "><div class="desc"><h2>' + titulo + '</h2><p>' + desc + '</p></div><div class="row"><div class="col-sm-4 text center"><a href="#"><i class="fab fa-facebook-square fa-2x"></i></i></a><a href="#"><i class="fab fa-twitter-square fa-2x"></i></i></a><a href="#"><i class="fab fa-google-plus-square fa-2x"></i></i></a></div></div></div></div>"')
+
+
+                ;
+
+carga1:false;
+
+            });
+
+
+    $.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/e125e4d7/json/json.json", function (json) {
 
             console.log(json);
             $.each(json, function (i, noticia) {
@@ -49,26 +83,18 @@ $(function () {
 
             });
 
+            carga2=false;
+        });
+
+
+        /*$.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/e125e4d7/json/json.json", function (json) {
+
+            
+
 
         });
 
-        $.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/e125e4d7/json/json.json", function (json) {
-
-            console.log(json);
-            $.each(json, function (i, noticia) {
-                var titulo = noticia.titulo;
-                var imagen = noticia.imagen;
-                var desc = noticia.descripcion;
-
-                $("#masnoticias").append('"<div class="row "><div class="col-sm-4 miniatura"><a href="not-cardio.html"><img class="img-fluid " src=' + imagen + ' class="rounded " alt="Cinque Terre "></a></div><div class="col-sm-8" style="background-color:silver; "><div class="desc"><h2>' + titulo + '</h2><p>' + desc + '</p></div><div class="row"><div class="col-sm-4 text center"><a href="#"><i class="fab fa-facebook-square fa-2x"></i></i></a><a href="#"><i class="fab fa-twitter-square fa-2x"></i></i></a><a href="#"><i class="fab fa-google-plus-square fa-2x"></i></i></a></div></div></div></div>"')
-
-
-                ;
-
-
-
-            });
-        });
+        */
 
         var popup = $('#popup');
         popup.css({
@@ -81,8 +107,9 @@ $(function () {
     //coso
 
 
-    function cargado() {
+    function cargado1() {
 
+        carga1=false;
 
 
         $.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/e125e4d7/json/json.json", function (json) {
@@ -108,6 +135,47 @@ $(function () {
             'left': ($(window).width() / 2 - $(popup).width() / 2) + 'px',
             'top': ($(window).height() / 2 - $(popup).height() / 2) + 'px'
         });
+
+        
+            
+        
+
+
+
+
+    }
+
+    function cargado2() {
+
+        carga2=false;
+
+        $.getJSON("https://cdn.rawgit.com/PedroDpsweb/WebNoticiasPedoSanchez/37bfa21e/json/json2.json", function (json) {
+
+            console.log(json);
+            $.each(json, function (i, noticia) {
+                var titulo = noticia.titulo;
+                var imagen = noticia.imagen;
+                var desc = noticia.descripcion;
+
+                $("#masnoticias").append('"<div class="row "><div class="col-sm-4 miniatura"><a href="not-cardio.html"><img class="img-fluid " src=' + imagen + ' class="rounded " alt="Cinque Terre "></a></div><div class="col-sm-8" style="background-color:silver; "><div class="desc"><h2>' + titulo + '</h2><p>' + desc + '</p></div><div class="row"><div class="col-sm-4 text center"><a href="#"><i class="fab fa-facebook-square fa-2x"></i></i></a><a href="#"><i class="fab fa-twitter-square fa-2x"></i></i></a><a href="#"><i class="fab fa-google-plus-square fa-2x"></i></i></a></div></div></div></div>"')
+
+
+                ;
+
+
+
+            });
+        });
+
+        var popup = $('#popup');
+        popup.css({
+            'left': ($(window).width() / 2 - $(popup).width() / 2) + 'px',
+            'top': ($(window).height() / 2 - $(popup).height() / 2) + 'px'
+        });
+
+        
+            
+        
 
 
 
